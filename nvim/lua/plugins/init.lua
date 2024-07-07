a25b962707f6abb53da3f63d8962527d7dfc7b6b
Blake Wilson <blake@blake.id>
2024-07-07 00:11:11 -0500

    Add formatters

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,87 +1,116 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre' -- uncomment for format on save
-    config = function()
-      require "configs.conform"
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>f",
+        function()
+          require("conform").format { async = true, lsp_format = "fallback" }
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
   },
 
- {
-   "neovim/nvim-lspconfig",
-   config = function()
-     require("nvchad.configs.lspconfig").defaults()
-     require "configs.lspconfig"
-   end,
- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
 
- {
- 	"williamboman/mason.nvim",
- 	opts = {
- 		ensure_installed = {
- 			"lua-language-server",
-      "stylua",
- 			"html-lsp",
-      "css-lsp" ,
-      "prettier",
-      "typescript-language-server",
-      "svelte-language-server",
-      "tailwindcss-language-server",
-      "markdownlint",
-      "json-lsp",
-      "html-lsp",
-      "eslint-lsp",
-      "codespell",
-      "bash-language-server",
-      "graphql-language-service-cli"
- 		},
- 	},
- },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "typescript-language-server",
+        "svelte-language-server",
+        "tailwindcss-language-server",
+        "markdownlint",
+        "json-lsp",
+        "html-lsp",
+        "eslint-lsp",
+        "codespell",
+        "bash-language-server",
+        "graphql-language-service-cli",
+      },
+    },
+  },
 
-{
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup {
         config = {
           week_header = {
-            enable = true
+            enable = true,
           },
-          shortcut = {
-          },
-        }
-    }
-  end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
-},
+          shortcut = {},
+        },
+      }
+    end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  },
 
-{
+  {
     "toppair/peek.nvim",
     event = { "VeryLazy" },
     build = "deno task --quiet build:fast",
     config = function()
-        require("peek").setup()
-        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
-}
+  }
 
- -- require('telescope').setup{
- --   defaults = {
- --     file_ignore_patterns = {
- --       "node_modules",
- --       "dist"
- --     }
- --   }
- -- }
+  -- require('telescope').setup{
+  --   defaults = {
+  --     file_ignore_patterns = {
+  --       "node_modules",
+  --       "dist"
+  --     }
+  --   }
+  -- }
 
- -- {
- -- 	"nvim-treesitter/nvim-treesitter",
- -- 	opts = {
- -- 		ensure_installed = {
- -- 			"vim", "lua", "vimdoc",
- --      "html", "css"
- -- 		},
- -- 	},
- -- },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
