6588f264c0a4f6adc15c438f79174df472cc350d
Blake Wilson <blake@blake.id>
2024-07-15 15:36:56 -0500

    Add faster key repeat

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,5 +1,9 @@
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Use scroll gesture with the Ctrl (^) modifier key to zoom
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
