a99c02b7011cdc1ad7d4bc36559ec9a97c1e9228
Blake Wilson <blake@blake.id>
2024-07-16 10:05:32 -0500

    Add auto nvm node version based on .nvmrc

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -49,6 +49,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Load .nvmrc config
+source $HOME/dotfiles/zsh/nvmrc.zsh
+
 # bun completions
 [ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
 
