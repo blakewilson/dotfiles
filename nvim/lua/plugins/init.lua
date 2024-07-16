073b7a79736afec70ba1d350b355b2a609057203
Blake Wilson <blake@blake.id>
2024-07-16 09:30:19 -0500

    Add diffview plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -56,7 +56,17 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup {
+        current_line_blame = true,
+      }
+    end,
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("diffview").setup()
     end,
   },
 
