d4b0acf5781ca1e4e27d15b1087588ed289a2fff
Blake Wilson <blake@blake.id>
2024-07-17 17:27:36 -0500

    Add my aliases

---
+++ zsh/aliases.zsh
@@ -0,0 +1,15 @@
+# Tmux
+alias ta="tmux attach -t"
+alias td="tmux detach"
+alias tk="tmux kill-session -t"
+
+# Show human readable list of all files
+alias l='ls -lFh'
+
+alias reload="source ~/.zshrc"
+
+# Git
+alias gaa="git add --all"
+
+# NPM
+alias nmd="npx npkill"
--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -56,6 +56,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Load aliases
+source $HOME/dotfiles/zsh/aliases.zsh
+
 # Load .nvmrc config
 source $HOME/dotfiles/zsh/nvmrc.zsh
 
