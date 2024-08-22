2ae84fabbf8f71b8ffe610457c59f2892fafeaa6
Blake Wilson <blake@blake.id>
2024-08-22 09:41:46 -0500

    Relative path + file name in status bar nvim

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,14 +1,32 @@
 ---@type ChadrcConfig
 local M = {}
 
+-- For the full path file name in the status bar
+vim.cmd "highlight St_relativepath guifg=#626a83 guibg=#2a2b36"
+local stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
 M.ui = {
   -- Other themes I like: yoru, tokyodark
-  theme = "tomorrow_night",
+  theme = "ayu_dark",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  -- Use the full relative file path for the file name in the status bar
+  statusline = {
+    theme = "default",
+    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
+
+        if path == "" then
+          return ""
+        end
+
+        return "%#St_relativepath#  " .. vim.fn.expand "%:.:h" .. " /"
+      end,
+    },
+  },
 }
 
 return M
